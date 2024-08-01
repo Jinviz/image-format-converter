2bc9ca7075d09d52c2529d3449a4eb85ea2d8d8c
Jinwoo Kim <rlawlsdn9583@gmail.com>
2024-08-01 11:35:37 +0900

    feat: before seperate logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
 
 function App() {
+  const [files, setFiles] = useState([]);
+  const [count, setCount] = useState(0)
+
+  const handleFileChange = (e) => {
+    setFiles(e.target.files);
+  };
+
+  const convertImageToPNG = (file) => {
+    setCount(count => count + 1);
+    console.log('convertImageToPNG ', count)
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        const img = new Image();
+        img.src = reader.result;
+        img.onload = () => {
+          const canvas = document.createElement('canvas');
+          canvas.width = img.width;
+          canvas.height = img.height;
+          const ctx = canvas.getContext('2d');
+          ctx.drawImage(img, 0, 0);
+          canvas.toBlob((blob) => {
+            resolve({ name: file.name.replace(/\.[^/.]+$/, '') + '.png', blob });
+          }, 'image/png');
+        };
+        img.onerror = (error) => reject(error);
+      };
+      reader.onerror = (error) => reject(error);
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const zip = new JSZip();
+    for (let i = 0; i < files.length; i++) {
+      try {
+        const convertedFile = await convertImageToPNG(files[i]);
+        zip.file(convertedFile.name, convertedFile.blob);
+      } catch (error) {
+        console.error('Error converting file:', error);
+      }
+    }
+    zip.generateAsync({ type: 'blob' })
+      .then((content) => {
+        saveAs(content, 'converted_images.zip');
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>JPG to PNG Converter</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="file" multiple accept=".jpg,.jpeg" onChange={handleFileChange} />
+        <button type="submit">Convert to PNG and Download as ZIP</button>
+      </form>
     </div>
   );
 }
---
+++ src/pages/MainPage/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+const MainPage = () => {
+  return (
+    <div>
+      asd
+    </div>
+  )
+}
+
+export default MainPage
+
+
+
+
