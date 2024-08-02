7b9471dabc31f05cf601619be89c5be31a19fe4b
Jinwoo Kim <rlawlsdn9583@gmail.com>
2024-08-02 17:52:02 +0900

    feat: 변환 로직 추가

--- src/components/ConvertFormat.js
+++ src/components/ConvertFormat.js
@@ -4,6 +4,8 @@ import FileSelect from './FileSelect'
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import FormatSelect from './FormatSelect';
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
 
 
 // 50개 단위로 Convert 후 다운로드 -> 남은 파일 반복
@@ -12,9 +14,49 @@ const ConvertFormat = () => {
     const [files, setFiles] = useState([]);
     const [format, setFormat] = useState('');
 
+    const convertImageToPNG = (file) => {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.readAsDataURL(file);
+          reader.onload = () => {
+            const img = new Image();
+            img.src = reader.result;
+            img.onload = () => {
+              const canvas = document.createElement('canvas');
+              canvas.width = img.width;
+              canvas.height = img.height;
+              const ctx = canvas.getContext('2d');
+              ctx.drawImage(img, 0, 0);
+              canvas.toBlob((blob) => {
+                resolve({ name: file.name.replace(/\.[^/.]+$/, '') + '.png', blob });
+              }, 'image/png');
+            };
+            img.onerror = (error) => reject(error);
+          };
+          reader.onerror = (error) => reject(error);
+        });
+      };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const zip = new JSZip();
+        for (let i = 0; i < files.length; i++) {
+          try {
+            const convertedFile = await convertImageToPNG(files[i]);
+            zip.file(convertedFile.name, convertedFile.blob);
+          } catch (error) {
+            console.error('Error converting file:', error);
+          }
+        }
+        zip.generateAsync({ type: 'blob' })
+          .then((content) => {
+            saveAs(content, 'converted_images.zip');
+          });
+      };
+
     return (
-        <form> 
-            <FileSelect setFiles={setFiles} setFormat={setFormat} />
+        <form onSubmit={handleSubmit}> 
+            <FileSelect setFiles={setFiles} />
             <FormatSelect format={format} setFormat={setFormat} />
             <button className={PulseBtn.animated} type="submit">
                 포맷 변환
