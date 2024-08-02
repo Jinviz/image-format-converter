ee6a3a12040ef4608dd446d5bdda85a726faf80d
Jinwoo Kim <rlawlsdn9583@gmail.com>
2024-08-02 17:39:29 +0900

    feat: 변환 포맷 선택 콤보박스

---
+++ src/components/FormatSelect.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const FormatSelect = ({format, setFormat}) => {
+
+    const handleChange = (e) =>{
+        setFormat(e.target.value)
+    }
+    return (
+        <div>
+            <label htmlFor="comboBox">선택하세요:</label>
+            <select id="comboBox" value={format} onChange={handleChange}>
+                <option value="">선택하세요</option>
+                <option value="jpg">jpg</option>
+                <option value="jpeg">jpeg</option>
+                <option value="png">png</option>
+                <option value="peg">peg</option>
+                <option value="gif">gif</option>
+                <option value="webp">webp</option>
+                <option value="svg">svg</option>
+                <option value="ico">ico</option>
+                <option value="bmp">bmp</option>
+            </select>
+        </div>
+    )
+}
+
+export default FormatSelect
